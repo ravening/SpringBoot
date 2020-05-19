70a04f5eda89988cd5c9280bd730d8dbbb55c952
Rakesh Venkatesh <rakeshv.com@gmail.com>
2020-05-19 12:45:18 +0200

    Reactive kafka project

---
+++ reactive-kafka/paymentprocessor-kafka-intro/paymentprocessor-gateway/src/main/java/com/rakeshv/paymentprocessor/gateway/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.rakeshv.paymentprocessor.gateway.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String getIndex() {
+        return "index";
+    }
+}
