a30b4e7bb30f1894c63fd25bea1cbf95c6dfe681
Rakesh Venkatesh <rakeshv.com@gmail.com>
2020-05-19 10:25:43 +0200

    Streaming twitter tweets

---
+++ twiter-streaming/twitter/twitter-backend/src/main/resources/static/app.js
@@ -0,0 +1,13 @@
+function connect() {
+  ws = new WebSocket('ws://localhost:8080/tweets');
+  ws.onmessage = function (data) {
+    console.log(data);
+    addToUi(data.data);
+  }
+}
+
+function addToUi(message) {
+  var jsonMsg = JSON.parse(message);
+  console.log(jsonMsg);
+  document.querySelector('#messages').innerHTML += "<tr><th>" + jsonMsg.status.user.screenName + "</th><th>"+jsonMsg.status.text+"</th></tr>";
+}
