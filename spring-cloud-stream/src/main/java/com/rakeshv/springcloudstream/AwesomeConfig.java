8b462535c6b39437d90d27271a99a2f71f08e270
Rakesh Venkatesh <rakeshv.com@gmail.com>
2021-01-27 17:42:34 +0100

    ifinances

--- spring-cloud-stream/src/main/java/com/rakeshv/springcloudstream/AwesomeConfig.java
+++ spring-cloud-stream/src/main/java/com/rakeshv/springcloudstream/AwesomeConfig.java
@@ -1,2 +1,15 @@
-package com.rakeshv.springcloudstream;public class AwesomeConfig {
+package com.rakeshv.springcloudstream;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.util.function.Supplier;
+
+@Configuration
+public class AwesomeConfig {
+    @Bean
+    public Supplier<String> awesomeBean() {
+        return () -> LocalDateTime.now().toString();
+    }
 }
